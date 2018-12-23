de1d5240f763b841bba5f7d874581c4b7ec4efa4
test <test1>
2018-12-23 11:47:04 +0900

    어댑터 클래스 기초 구조 작성

--- app/src/main/java/kr/tjeit/a20181216_baemincopy/adapters/RestaurantAdapter.java
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/adapters/RestaurantAdapter.java
@@ -1,4 +1,37 @@
 package kr.tjeit.a20181216_baemincopy.adapters;
 
-public class RestaurantAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.tjeit.a20181216_baemincopy.R;
+import kr.tjeit.a20181216_baemincopy.datas.Restaurant;
+
+public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+
+    Context mContext;
+    List<Restaurant> mList;
+    LayoutInflater inf;
+
+    public RestaurantAdapter(Context mContext, List<Restaurant> mList){
+        super(mContext, R.layout.restaurant_list_item, mList);
+        this.mContext = mContext;
+        this.mList = mList;
+        this.inf = LayoutInflater.from(mContext);
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if(row == null){
+            row = inf.inflate(R.layout.restaurant_list_item, null);
+        }
+
+        return row;
+    }
 }
